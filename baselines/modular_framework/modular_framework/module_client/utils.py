12ceb940ee7d8483202ce7416da6756d57003863
Axi404 <axihelloworld@gmail.com>
2025-11-12 13:58:28 +0800

    [feat] remove archived baselines

--- baselines/modular_framework/modular_framework/module_client/utils.py
+++
@@ -1,9 +0,0 @@
-def process_check_finished(result):
-    while isinstance(result, dict):
-        if "finished" in result:
-            result = result["finished"]
-        else:
-            for res in result:
-                result = result[res]
-                break
-    return result
